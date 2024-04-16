f90d1d56b459d09175895fa842eee78c02b8fdbf
Dmitry <dimonik1337@gmail.com>
2024-04-16 17:25:56 +0500

    done counter

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,39 +1,55 @@
 (function(){
-    let plusCollection = document.getElementsByClassName("dropdown-list__plus");
-    let minusCollection = document.getElementsByClassName("dropdown-list__minus");
-    let accomodationCollection = document.getElementsByClassName("dropdown-list__quantity");
-
-
-    for (let i = 0; i < plusCollection.length; i++){
-        plusCollection.item(i).count = 0;
-        plusCollection.item(i).addEventListener("click", counter(this.item(i).count));
-        minusCollection.item(i).addEventListener("click", counter);
-        //accomodationCollection.item(i).count = 0;
-    }
-        
-    function countHandler(event){
-        if (event.target.attributes.class.nodeValue == "dropdown-list__plus"){
-            console.log("win");
-            return event
+    let plusColl = document.getElementsByClassName("dropdown-list__plus");
+    let minusColl = document.getElementsByClassName("dropdown-list__minus");
+    let accomodationColl = document.getElementsByClassName("dropdown-list__quantity");
+    let textField = document.getElementsByClassName("dropdown__input-text");
+    class Counter{
+        constructor(plus, minus, counter, text, number, inputText){
+            this.number = number;
+            this.count = 0;
+            this.text = inputText;
+            this.plusButton = plus.item(number);
+            this.minusButton = minus.item(number);
+            this.counterButton = counter.item(number);
+            if (number <= 2){
+                this.textField = text.item(0);}
+            else { 
+                this.textField = text.item(1);}
+            this.plusButton.addEventListener("click", this.add.bind(this));
+            this.minusButton.addEventListener("click", this.subtract.bind(this));
         }
-        console.log(event);
-        accomodationCollection.item(this).count++;
-        this.previousSibling.innerHTML = accomodationCollection.item(this).count;
+
+        add(){
+            this.count++;
+            this.counterButton.innerText = this.count;
+            return this.update();
+        }
+
+        subtract(){
+            if (this.count > 0){
+                this.count--;
+                this.counterButton.innerText = this.count;
+            }
+                return this.update();
+            
+        }
+
+        update(){
+            if (this.number <= 1){
+                this.textField.innerText = this.count + " " + this.text;
+            }
+            else {this.textField.innerText = this.count + " " + this.text;}
+
+            return this.count
+        }
     }
 
-    function counter(count) {
-        return function(event){
-            console.log(event);
-            let count = 0;
-        console.log(count);
-        if (event.target.attributes.class.nodeValue == "dropdown-list__plus")
-            {return count++}
-        else {return count--}
-        
-        }
-        
-      }
-
+    let leCounter1 = new Counter(plusColl, minusColl, accomodationColl, textField, 0, "Спальни" );
+    let leCounter2 = new Counter(plusColl, minusColl, accomodationColl, textField, 1, "Кровати" );
+    let leCounter3 = new Counter(plusColl, minusColl, accomodationColl, textField, 2);
+    let leCounter4 = new Counter(plusColl, minusColl, accomodationColl, textField, 3, "Гостей");
+    let leCounter5 = new Counter(plusColl, minusColl, accomodationColl, textField, 4, "Гостей");
+    let leCounter6 = new Counter(plusColl, minusColl, accomodationColl, textField, 5, "Гостей");
 
 })() 
 

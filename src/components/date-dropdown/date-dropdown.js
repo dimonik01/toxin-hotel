2af550d35cef96119d25536c0d88e49aef6b7e80
Dmitry <dimonik1337@gmail.com>
2024-05-02 18:51:03 +0500

    updated date-dropdown

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -2,7 +2,10 @@
     let parameters = {
         buttons: document.getElementsByClassName("date-dropdown__expand"),
         dropdown: document.getElementsByClassName("factual-dd"),
+        
+
     }
+
     class DropdownToggler{
         constructor(object, number){
             this.button = object.buttons[number];
@@ -11,10 +14,76 @@
         }
 
         clickHandler(){
-            console.log("Masya is the BEST!!!!!!");
+            this.dropdown.classList.toggle("factual-dd");
+            this.dropdown.classList.toggle("factual-dd_closed");
         }
+    }
+    class Calendar{   
+        constructor(node){
+            this.node = node;
+            this.weekdays = ["ПН","ВТ","СР","ЧТ","ПН","СБ","ВС"];
+            this.weekdaysNode = document.getElementsByClassName("factual-dd__weekdays_active").item(0);
+            this.daysNode = document.getElementsByClassName("factual-dd__days_active").item(0);
+            this.dayValueNode = document.getElementsByClassName("factual-dd__day-value_active").item(0);
+            this.monthsNode = document.getElementsByClassName("factual-dd__months").item(0);
+            this.monthValueNode = document.getElementsByClassName("factual-dd__month-value").item(0);
+            this.yearValueNode = document.getElementsByClassName("factual-dd__year-value").item(0);
+            this.yearNode = document.getElementsByClassName("factual-dd__years").item(0);
+            this.acceptButton = document.getElementsByClassName("factual-dd__accept").item(0);
+            this.clearButton = document.getElementsByClassName("factual-dd__clear").item(0);
+            this.dayValueNode.addEventListener("click", this.showMonths.bind(this));
+            this.monthValueNode.addEventListener("click", this.showYears.bind(this));
+            this.yearValueNode.addEventListener("click", this.showDays.bind(this));
+        }
+
+        initDays(){
+            for (let i = 0; i < 7; i++){  //initialising weekdays
+                let newDiv = document.createElement("div");
+                newDiv.className = "factual-dd__days_active";
+                let newContent = document.createTextNode(this.weekdays[i]);
+                newDiv.appendChild(newContent);
+                this.weekdaysNode.appendChild(newDiv);
+            }
+        }
+
+        initCells(){
+            for (let i = 1; i <= 31; i++){ //initialising dates
+                console.log("test");
+                let newDiv = document.createElement("div");
+                newDiv.className = "factual-dd__cell";
+                let newContent = document.createTextNode(i);
+                newDiv.appendChild(newContent);
+                this.daysNode.appendChild(newDiv);
+            } 
+        }
+        showMonths(){
+            this.daysNode.classList.replace("factual-dd__days_active","factual-dd__days");
+            this.weekdaysNode.classList.replace("factual-dd__weekdays_active","factual-dd__weekdays");
+            this.dayValueNode.classList.replace("factual-dd__day-value_active","factual-dd__day-value");
+            this.monthValueNode.classList.replace("factual-dd__month-value","factual-dd__month-value_active");
+            this.monthsNode.classList.replace("factual-dd__months","factual-dd__months_active");
+            this.acceptButton.classList.replace("factual-dd__accept","factual-dd__accept_hidden");
+            this.clearButton.classList.replace("factual-dd__clear","factual-dd__clear_hidden");
+        }
+
+        showYears(){
+            this.monthValueNode.classList.replace("factual-dd__month-value_active","factual-dd__month-value" );
+            this.yearValueNode.classList.replace("factual-dd__year-value","factual-dd__year-value_active");
+            this.yearValueNode.classList.replace("factual-dd__year-value","factual-dd__year-value_active");
+            this.monthsNode.classList.replace("factual-dd__months_active","factual-dd__months");
+            this.yearNode.classList,replace("factual-dd__years", "factual-dd__years_active");
+
+        }
+
+        showDays(){
+
+        }
+
     }
     let button1 = new DropdownToggler(parameters, 0);
     let button2 = new DropdownToggler(parameters, 1);
+    let calendar = new Calendar();
+    calendar.initCells();
+    calendar.initDays();
 
-})()
+})();

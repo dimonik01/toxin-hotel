112df2ea59d33f7810e022528b879fb390f6a6b0
Dmitry <dimonik1337@gmail.com>
2024-06-21 16:32:27 +0500

    buttons component done

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -1,6 +1,8 @@
 (function(){
     class Range{
         constructor(node){
+            this.range = document.getElementsByClassName("range__slider").item(node);
+            this.rangeCoords = this.range.getBoundingClientRect();
             this.first = document.getElementsByClassName("range__slider-dot_first").item(node);
             this.second = document.getElementsByClassName("range__slider-dot_second").item(node);
             this.firstCoords = this.first.getBoundingClientRect().x;
@@ -12,7 +14,7 @@
         }
             rangeHandler(e){
                 e.preventDefault();
-                const bound = move.bind(this);
+                const bound = moveHandler.bind(this);
                 const unbound = removeHandler.bind(this);
                 if (e.target.className == "range__slider-dot range__slider-dot_first"){
                     e.finalTarget = this.first;
@@ -37,56 +39,75 @@
                     console.log(this.firstCoords + " <= " + this.secondCoords);
                     
                 }
-                function move(f){
-                    if (this.secondCoords > 600 && isOn == false){
-                        document.getElementsByClassName("pro").item(0).className = "pro_on";
-                        isOn = true
-                    }
+                function moveHandler(f){
                     if (this.firstCoords <= this.secondCoords - 16){
                         if (this.moveCount >= 1){
-                            e.finalTarget.style.left = f.pageX + 'px';
-                            this.moveCount = 0;
-                            this.firstCoords = this.first.getBoundingClientRect().x;
-                            this.secondCoords = this.second.getBoundingClientRect().x;
-                            console.log("default");
-                            console.log(this.firstCoords + " <= " + this.secondCoords);
+                            console.log(this.firstCoords + "  " + this.rangeCoords.left);
+                            console.log(this.secondCoords + "  " + this.rangeCoords.right);
+                            move.call(this, e.finalTarget, f);
+                            boundsCheck.call(this, e.finalTarget, f);
                         }
                     }
                     else{
                         if (e.finalTarget == this.first){
                             if(f.clientX < this.firstCoords){
                                 if (this.moveCount >= 1){
-                                    e.finalTarget.style.left = f.pageX + 'px';
-                                    this.moveCount = 0;
-                                    this.firstCoords = this.first.getBoundingClientRect().x;
-                                    console.log("first non-default");
-                                    console.log("bigger");
+                                    move.call(this, e.finalTarget, f);
+                                    boundsCheck.call(this, e.finalTarget, f);
                                 }
                             }
                             else {
-                                e.finalTarget.style.left = this.secondCoords - 16 + 'px';
+                                e.finalTarget.style.left = this.secondCoords - 16 - this.rangeCoords.x + 'px';
                                 console.log("else first");
                             }
                         }
                         else{
                             if(f.clientX - 16 > this.firstCoords){
                                 if (this.moveCount >= 1){
-                                    e.finalTarget.style.left = f.pageX + 'px';
-                                    this.moveCount = 0;
-                                    this.secondCoords = this.second.getBoundingClientRect().x;
-                                    console.log("second non-default");
-                                    console.log(f.clientX - 16 + ">" + this.firstCoords);
+                                    move.call(this, e.finalTarget, f);
+                                    boundsCheck.call(this, e.finalTarget, f);
                                 }
                             }
                             else {
-                                e.finalTarget.style.left = this.firstCoords + 16  + 'px';
+                                e.finalTarget.style.left = this.firstCoords + 16 - this.rangeCoords.x + 'px';
                                 console.log("else second");
                             }
                         }
-                            
+                                         
                     }
                     this.moveCount++;
+                    function move(finalTarget, f){
+                        console.log(this);
+                        console.log("triggered");
+                        console.log(finalTarget);
+                        console.log(f);
+                        finalTarget.style.left = f.pageX - this.rangeCoords.x + 'px';
+                        this.moveCount = 0;
+                        this.firstCoords = this.first.getBoundingClientRect().x;
+                        this.secondCoords = this.second.getBoundingClientRect().x;
+                    }
+                    
+                    function boundsCheck(finalTarget, f){
+                        if (finalTarget == this.first){
+                            if (this.firstCoords <= this.rangeCoords.left - 6){
+                                console.log("left bounds" + finalTarget.style.left + "  " + this.rangeCoords.left);
+                                console.log(finalTarget);
+                                finalTarget.style.left = -6 + "px"
+                                console.log("left bounds" + finalTarget + "  " + this.rangeCoords.left);
+                            }
+                        }
+                        else{
+                            console.log("range right =  " + this.rangeCoords.right);
+                            if (this.secondCoords >= this.rangeCoords.right - 16){
+                                console.log("right bounds" + finalTarget.style.left + "  " + this.rangeCoords.right);
+                                console.log(finalTarget);
+                                finalTarget.style.left = this.rangeCoords.right - this.rangeCoords.left - 16 + "px";
+                                console.log("right bounds" + finalTarget + "  " + this.rangeCoords.right);
+                            }
+                        }
+                    }
                 }
+
             }
     }
     let range = new Range(0);

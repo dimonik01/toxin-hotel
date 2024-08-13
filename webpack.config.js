b92ecec9c297b0c1870f1a0068c82b918e546716
Dmitry <dimonik1337@gmail.com>
2024-08-13 17:03:40 +0500

    added landing page

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
     colorsAndType: './src/pages/colors-and-type/colors-and-type.pug',
     formElements: './src/pages/form-elements/form-elements.pug',
     headersAndFooters: './src/pages/headers-and-footers/headers-and-footers.pug',
-    cards: './src/pages/cards/cards.pug'
+    cards: './src/pages/cards/cards.pug',
+    landing: './src/pages/landing/landing.pug'
     // ...
   },
 

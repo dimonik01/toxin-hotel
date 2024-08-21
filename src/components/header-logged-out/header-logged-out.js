5596099b9dac6beafa8f00dde4bb55b8b3570949
Dmitry <dimonik1337@gmail.com>
2024-08-21 10:35:21 +0500

    landing responsiveness done

--- src/components/header-logged-out/header-logged-out.js
+++ src/components/header-logged-out/header-logged-out.js
@@ -4,17 +4,23 @@ class Mobile{
         this.button = document.getElementsByClassName("header-logged-out__menu_inactive").item(0);
         this.active = false;
         this.menu = document.getElementsByClassName("header-logged-out__mobile-menu").item(0);
+        this.submenu = document.getElementsByClassName("mobile_hidden").item(0);
+        this.list = document.getElementsByClassName("mobile__list_hidden").item(0);
         this.button.addEventListener("click", this.menuHandler.bind(this));
     }
     menuHandler(){
         console.log("done");
         if (this.active == false){
             this.menu.classList.replace("header-logged-out__mobile-menu_inactive", "header-logged-out__mobile-menu_active");
+            this.submenu.classList.replace("mobile_hidden","mobile");
+            this.list.classList.replace("mobile__list_hidden","mobile__list");
             this.active = true;
             console.log("false");
         }
         else{
             this.menu.classList.replace("header-logged-out__mobile-menu_active","header-logged-out__mobile-menu_inactive");
+            this.submenu.classList.replace("mobile","mobile_hidden");
+            this.list.classList.replace("mobile__list","mobile__list_hidden");
             this.active = false;
             console.log("true");
         }
